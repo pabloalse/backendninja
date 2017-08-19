506d74400822ade18645269fb94d8e2a1ac0ee38
Juan Pablo <Juan Pablo@LAPTOP-BHVMPQUG>
2017-08-19 17:21:43 +0100

    Creacion de converter

---
+++ backendninja/src/main/java/com/udemy/model/CourseModel.java
@@ -0,0 +1,46 @@
+package com.udemy.model;
+
+
+public class CourseModel {
+
+	private String name;
+	private String description;
+	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	private int hours;
+
+	public CourseModel(String name, String description, int price, int hours) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.hours = hours;
+	}
+	
+	public CourseModel() {}
+
+}
