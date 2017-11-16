9d919805c421171502ac76b145241bbbe676b02e
Juan Pablo <Juan Pablo@LAPTOP-BHVMPQUG>
2017-11-16 19:07:25 +0000

    BackNinja2 primera subida
    Change-Id: Ia8253a93b49ba0dbd671ebb8264a32a52fa3a6c4
    Signed-off-by: Juan Pablo <Juan Pablo@LAPTOP-BHVMPQUG>

---
+++ backendninja2/src/main/java/com/udemy/constant/ViewConstant.java
@@ -0,0 +1,8 @@
+package com.udemy.constant;
+
+public class ViewConstant {
+
+	public static final String CONTANT_FORM = "contactform";
+	public static final String CONTANTS = "contacts";
+	public static final String LOGIN = "login";
+}
---
+++ backendninja2/src/test/java/com/udemy/prueba.java
@@ -0,0 +1,19 @@
+package com.udemy;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class prueba {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
