6c0bf1a6de368ca54306fbf25ac2508f5fc9061a
Juan Pablo <pabloalse@hotmail.com>
2018-03-21 20:28:39 +0000

    casi finalizado

--- backendninja2/src/main/java/com/udemy/controller/ContactController.java
+++ backendninja2/src/main/java/com/udemy/controller/ContactController.java
@@ -4,6 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +35,7 @@ public class ContactController {
 		return "redirect:/contacts/showcontacts";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/contactform")
 	private String redirectContactForm(@RequestParam(name="id", required=false) int id, Model model){
 		ContactModel contact = new ContactModel();
@@ -59,6 +63,8 @@ public class ContactController {
 	@GetMapping("/showcontacts")
 	public ModelAndView showContacts(){
 		ModelAndView mov = new ModelAndView(ViewConstant.CONTACTS);
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		mov.addObject("username", user.getUsername());
 		mov.addObject("contacts", contactService.listAllContacts());
 		return mov;
 	} 
